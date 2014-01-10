648a8649c24ea9c13d2333cd92c14c7d49b51f59
LoreLLo <lorenzo.salvadorini@softecspa.it>
2014-01-10 15:15:55 +0100

    Added setup script

--- README.md
+++ README.md
@@ -6,6 +6,10 @@ My notebook setup using Puppet and basic idea from GitHub Boxen
 
 Look at [the original Boxen](http://boxen.github.com/) to understand what it is and how to use it.
 
+Setup
+-----
+    wget -O- -q https://raw2.github.com/lorello/ubuntu-boxen/master/install.sh | /bin/bash
+
 
 [![Bitdeli Badge](https://d2weczhvl823v0.cloudfront.net/lorello/ubuntu-boxen/trend.png)](https://bitdeli.com/free "Bitdeli Badge")
 
---
+++ install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "This script requires root privileges, you will be asked your sudo password"
+
+
+# Setup PuppetLabs repository
+DISTRO=$(grep DISTRIB_CODENAME /etc/lsb-release | awk -F= '{print $2}')
+wget -q https://apt.puppetlabs.com/puppetlabs-release-$DISTRO.deb
+sudo dpkg -i puppetlabs-release-$DISTRO.deb
+sudo apt-get update -y -q
+
+# Install puppet without the agent init script
+sudo apt-get install puppet-common -y -q
+
+
