55b6a6a6a47ec5720b66b595cc56ac84e1bde77c
lorello <lorenzo.salvadorini@softecspa.it>
2015-06-12 02:59:22 +0200

    better vagrant support

--- uboxen.pp
+++ uboxen.pp
@@ -261,35 +261,77 @@ node generic_desktop {
 }
 
 
-class profile::vagrant {
+class profile::vagrant(
+    $manage_repo  = false,
+    $manage_shell = false,
+    $boxes_domain = undef,
+    $plugins      = {},
+    $user         = undef,
+) {
 
-    apt::source { 'wolfgang42-vagrant':
-        comment  => 'This is an unofficial .deb repository for Vagrant, hosted by Wolfgang Faust.',
-        location => 'http://vagrant-deb.linestarve.com/',
-        release  => 'any',
-        repos    => 'main',
-        key      => {
-            'id' => '2099F7A4',
-        },
-    }->
+    validate_bool($manage_repo)
+    validate_bool($manage_shell)
+    validate_hash($plugins)
+
+    if $manage_repo {
+        apt::source { 'wolfgang42-vagrant':
+            comment  => 'This is an unofficial .deb repository for Vagrant, hosted by Wolfgang Faust.',
+            location => 'http://vagrant-deb.linestarve.com/',
+            release  => 'any',
+            repos    => 'main',
+            key      => {
+                'id' => '2099F7A4',
+            },
+        }
+    }
+
     package { 'vagrant':    ensure => latest }
 
     package { 'virtualbox': ensure => latest }
 
-    wget::fetch { 'vagrant-bash-completion':
-        source      => 'https://github.com/kura/vagrant-bash-completion/raw/master/vagrant',
-        destination => '/etc/bash_completion.d/vagrant',
-        cache_dir   => '/var/cache/wget',
+    if $manage_shell {
+        wget::fetch { 'vagrant-bash-completion':
+            source      => 'https://github.com/kura/vagrant-bash-completion/raw/master/vagrant',
+            destination => '/etc/bash_completion.d/vagrant',
+            cache_dir   => '/var/cache/wget',
+        }
+
+        bash::rc { 'alias vu="vagrant up"' : }
+        bash::rc { 'alias vp="vagrant provision"' : }
+        bash::rc { 'alias vs="vagrant suspend"' : }
+    }
+    if $boxes_domain {
+        dnsmasq::conf { "resolve-${boxes_domain}":
+            content  => 'address=/${boxes_domain}/127.0.1.1',
+        }
     }
 
-    bash::rc { 'alias vu="vagrant up"' : }
-    bash::rc { 'alias vp="vagrant provision"' : }
-    bash::rc { 'alias vs="vagrant suspend"' : }
-
-    dnsmasq::conf { 'resolve-vagrant.local':
-        content  => 'address=/vagrant.local/127.0.1.1',
+    $plugin_defaults = {
+        ensure => present,
     }
 
+    if $plugins {
+        create_resources('profile::vagrant::plugin', $plugins, $plugin_defaults)
+    }
+}
+
+define profile::vagrant::plugin(
+    $ensure = present,
+) {
+
+    if $ensure == 'present' {
+        exec { "install-${name}":
+            command => "/usr/bin/vagrant plugin install vagrant-${name}",
+            unless  => "/usr/bin/vagrant plugin list | grep vagrant-${name}",
+            user    => $::profile::vagrant::user,
+        }
+    } else {
+        exec { "uninstall-${name}":
+            command => "/usr/bin/vagrant plugin uninstall vagrant-${name}",
+            onlyif  => "/usr/bin/vagrant plugin list | grep vagrant-${name}",
+            user    => $::profile::vagrant::user,
+        }
+    }
 }
 
 
