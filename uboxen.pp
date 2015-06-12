d1f8475aee752aae7a48354a82bc5496384b99fa
lorello <lorenzo.salvadorini@softecspa.it>
2015-06-12 08:02:21 +0200

    fixed error when installing vagrant plugins

--- uboxen.pp
+++ uboxen.pp
@@ -332,14 +332,16 @@ define profile::vagrant::plugin(
     if $ensure == 'present' {
         exec { "install-${name}":
             command => "/usr/bin/vagrant plugin install vagrant-${name}",
-            unless  => "/usr/bin/vagrant plugin list | grep vagrant-${name}",
+            unless  => "/usr/bin/sudo -u ${::profile::owner::username} /usr/bin/vagrant plugin list | grep vagrant-${name}",
             user    => $::profile::owner::username,
+            environment => "HOME=/home/${::profile::owner::username}",
         }
     } else {
         exec { "uninstall-${name}":
             command => "/usr/bin/vagrant plugin uninstall vagrant-${name}",
-            onlyif  => "/usr/bin/vagrant plugin list | grep vagrant-${name}",
-            user    => $::profile::owner::username,
+            onlyif  => "/usr/bin/sudo -u ${::profile::owner::username} /usr/bin/vagrant plugin list | grep vagrant-${name}",
+            user    => $::profile::owner::usernamett,
+            environment => "HOME=/home/${::profile::owner::username}",
         }
     }
 }
