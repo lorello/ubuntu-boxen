503514713b93aa894aa65ceef37a26e5ed456772
lorello <lorenzo.salvadorini@softecspa.it>
2015-08-28 10:18:01 +0200

    added missing config for docker service in systemctl

--- uboxen.pp
+++ uboxen.pp
@@ -41,6 +41,18 @@ class profile::docker {
         docker_users => [ $profile::owner::username ],
     }
 
+    # 25/8/2015 lorello
+    # missing config for docker in systemctl
+    # http://nknu.net/how-to-configure-docker-on-ubuntu-15-04/
+    if $lsbdistrelease == '15.04' {
+        file { '/etc/systemd/system/docker.service.d/ubuntu.conf':
+            content => "[Service]\nEnvironmentFile=/etc/default/docker\nExecStart=\nExecStart=/usr/bin/docker -d -H fd:// \$DOCKER_OPTS\n",
+        } ->
+        exec { 'systemctl daemon-reload': }
+        ->
+        exec { 'systemctl restart docker': }
+    }
+
 }
 
 # Puppet dev environment
